ceb5d83f562fcc7df3745ed9f4bc61a28d32b81d
kj1352 <nikhilkj24@gmail.com>
2017-06-24 16:17:48 +0530

    Splash Screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,19 @@
 angular.module('starter.controllers', [])
 
-.controller('HomeCtrl', function($scope, $state) {
-    $scope.dishdetails = function() {
-        $state.go('dishdetails');
-    }
-})
+    .controller('HomeCtrl', function ($scope, $state) {
+        $scope.$on('$ionicView.afterEnter', function () {
+            setTimeout(function () {
+                document.getElementById("custom-overlay").style.display = "none";
+            }, 3500);
+        });
 
-.controller('DishdetailsCtrl', function($scope, $state) {
-    $scope.showfeatures = function () {
-        $state.go('dishinstallation');
-    }
-})
+        $scope.dishdetails = function () {
+            $state.go('dishdetails');
+        }
+    })
+
+    .controller('DishdetailsCtrl', function ($scope, $state) {
+        $scope.showfeatures = function () {
+            $state.go('dishinstallation');
+        }
+    })
