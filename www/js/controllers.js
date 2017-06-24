cdd1c31346906dcd1cc29061d49be386f48aaa56
kj1352 <nikhilkj24@gmail.com>
2017-06-24 12:52:34 +0530

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope, $state) {
+    $scope.dishdetails = function() {
+        $state.go('dishdetails');
+    }
+})
+
+.controller('DishdetailsCtrl', function($scope, $state) {
+    $scope.showfeatures = function () {
+        $state.go('dishfeatures');
+    }
+})
